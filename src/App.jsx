550f50a7d7c4d5a3611a53a85e73c96ef033b39c
nicowyx <nico.wyx@gmail.com>
2024-06-26 08:18:45 -0300

    tudo acabará em pizza.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import './App.css'
-  import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem'
 
   const App = () => {
 
+    const [color] = useState("#0000FF")
+    document.body.style.backgroundColor = color
+
     return (
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Listagem />} />
       </Routes>
     )
   }
